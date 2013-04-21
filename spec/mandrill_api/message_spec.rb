544ee4dfb2f8744fa2538b7c9a4c928ee171be9a
bilalbash <bilal.basharat@gmail.com>
2013-04-21 05:24:55 +0500

    started rspec

--- spec/mandrill_api/message_spec.rb
+++ spec/mandrill_api/message_spec.rb
@@ -1,8 +1,11 @@
+require 'spec_helper'
+
 describe "message" do
   before :each do
     MandrillApi.connect_mandrill
   end
 
-  it "should get th"
-
+  it "should sent the message" do
+    pending
+  end
 end
---
+++ spec/mandrill_api/template_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "template" do
+  before :each do
+    MandrillApi.connect_mandrill
+  end
+
+  it "should return the list of templates" do
+    pending
+  end
+
+  it "should add list of user against template" do
+    pending
+  end
+end
